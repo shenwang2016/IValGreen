5f0fa61f06a05e91fd5ac5c398455341160014c1
ylh96 <ylh96@192.168.132.1>
2016-09-11 16:19:09 +0800

    added classes

--- src/IValGreen_Admin.java
+++ src/IValGreen_Admin.java
@@ -9,13 +9,6 @@
 public class IValGreen_Admin {
 
 	/**
-	 * 
-	 */
-	public IValGreen_Admin() {
-		// TODO Auto-generated constructor stub
-	}
-
-	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
--- src/IValGreen_User.java
+++ src/IValGreen_User.java
@@ -9,13 +9,6 @@
 public class IValGreen_User {
 
 	/**
-	 * 
-	 */
-	public IValGreen_User() {
-		// TODO Auto-generated constructor stub
-	}
-
-	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
