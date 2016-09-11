664ddc9ac3b535b1237f6b8d5764d8cc92f0042e
ylh96 <ylh96@192.168.132.1>
2016-09-11 16:12:25 +0800

    Added class

--- src/IValGreen_Admin.java
+++ src/IValGreen_Admin.java
@@ -3,18 +3,24 @@
  */
 
 /**
- * @author
- * Shen Wang, Yilun Hua
+ * @author ylh96
  *
  */
-public class IValGreen_Main {
+public class IValGreen_Admin {
+
+	/**
+	 * 
+	 */
+	public IValGreen_Admin() {
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
--- src/IValGreen_User.java
+++ src/IValGreen_User.java
@@ -3,18 +3,24 @@
  */
 
 /**
- * @author
- * Shen Wang, Yilun Hua
+ * @author ylh96
  *
  */
-public class IValGreen_Main {
+public class IValGreen_User {
+
+	/**
+	 * 
+	 */
+	public IValGreen_User() {
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
