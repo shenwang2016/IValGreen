78f44e0c91148e5f288f2eaf61664b20793fdc67
ylh96 <ylh96@192.168.132.1>
2016-09-13 17:57:42 +0800

    main addeed

--- src/IValGreen_Main.java
+++ src/IValGreen_Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * 
  */
@@ -8,13 +10,29 @@
  *
  */
 public class IValGreen_Main {
-
+	
+	static GreenLibrary gl = new GreenLibrary();
+	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter Award System:");
+		String file_am = scan.nextLine();
+		System.out.println("Enter Item List:");
+		String file_lm = scan.nextLine();
+		System.out.println("Enter Item Indexer:");
+		String file_im = scan.nextLine();
+		gl.set_awards(file_am);
+		gl.set_category_to_weight(file_lm);
+		gl.set_item_to_category(file_im);
+		scan.close();
+	}
+	
+	public static GreenLibrary getGL() {
+		return gl;
 	}
 
 }
