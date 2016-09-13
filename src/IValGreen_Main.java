890629567893eddac61d42d67bdafb6db8a2eaef
Shen Wang <swang05@uw.edu>
2016-09-13 06:15:08 -0700

    add comments

--- src/IValGreen_Main.java
+++ src/IValGreen_Main.java
@@ -19,14 +19,20 @@ public class IValGreen_Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scan = new Scanner(System.in);
+		// get path of award_system file
 		System.out.println("Enter Award System:");
 		String file_am = scan.nextLine();
+		// get path of item_list file
 		System.out.println("Enter Item List:");
 		String file_lm = scan.nextLine();
+		// get path of item_index file
 		System.out.println("Enter Item Indexer:");
 		String file_im = scan.nextLine();
+		// set awards map
 		gl.set_awards(file_am);
+		// set item_list map
 		gl.set_category_to_weight(file_lm);
+		// set item_index map
 		gl.set_item_to_category(file_im);
 		scan.close();
 	}
