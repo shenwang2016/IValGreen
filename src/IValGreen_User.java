59e9a47392115847cce7564630a8531bbd054def
Shen Wang <swang05@uw.edu>
2016-09-13 01:08:03 -0700

    modify and comments

--- src/IValGreen_User.java
+++ src/IValGreen_User.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
 
@@ -20,8 +21,11 @@ public class IValGreen_User {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		GreenAccount user_ga = null;
+		// am is a map of award_system
 		HashMap<String, String> am = null;
+		// lm is a map of item_list
 		HashMap<String, String> lm = null;
+		// im is a map of item_indexer
 		HashMap<String, String> im = null;
 		Scanner scan = new Scanner(System.in);
 		String username = scan.nextLine();
@@ -38,6 +42,8 @@ public class IValGreen_User {
 	public static GreenAccount login_user(GreenAccount ga) {
 		// if account not exist
 		if (ga == null) {
+			// enter here means account not exist
+			// ask user whether he is willing to create a new account
 			System.out.println("Account not exist");
 			System.out.println("Would you like to create a new account?");
 			System.out.print("Enter \"yes\" or \"no\": ");
@@ -63,8 +69,11 @@ public class IValGreen_User {
 	// create green account for user
 	public static GreenAccount create_account() {
 		Scanner scan = new Scanner(System.in);
+		// get user name from user
 		String username = scan.nextLine();
+		// get password from user
 		String password = scan.nextLine();
+		// create a new green account for user
 		GreenAccount ga = new GreenAccount(username, password, true);
 		// set security questions
 		ga.setSecurityQuestions();
@@ -76,6 +85,7 @@ public class IValGreen_User {
 	
 	// check whether user is logged in or not
 	public static boolean check_status() {
+		// get user name
 		Scanner scan = new Scanner(System.in);
 		String username = scan.nextLine();
 		// try to pull green account from user_info
@@ -87,21 +97,27 @@ public class IValGreen_User {
 		return ga.verifyID();
 	}
 	
+	// get award list, then print them out
 	public static boolean check_awards(GreenAccount ga) {
-		ga.getAwards(IValGreen_Admin.getAwardMap());
+		ArrayList<String> award_list = ga.getAwards(IValGreen_Admin.getAwardMap());
+		for(String award: award_list) {
+			System.out.println(award);
+		}
 		return true;
 	}
 	
 	// map item -> num
 	// num should be > 0
 	public static void add_contribution(String item_name, int num) {
+		// if green_record contains item name, then increase its previous count
 		if(green_record.containsKey(item_name)) {
 			int temp_num = green_record.get(item_name);
 			temp_num += num;
 			green_record.put(item_name, temp_num);
-		} else {
+		} else { // if not, then add item_name ->  num into green_record
 			green_record.put(item_name, num);
 		}
+		// adjust trace_contribution
 		trace_contribution += num;
 		
 	}
